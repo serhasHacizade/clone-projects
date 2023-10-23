6c0d13c8c7c7f1a50d24a1fce25a842112eb7d05
serhasHacizade <eren.felek.4@gmail.com>
2023-10-23 12:35:17 +0300

    commit

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -43,11 +43,15 @@ const Header: React.FC<HeaderProps> = ({
                 <div className="flex justify-between items-center gap-x-4">
                     <>
                         <div>
-                            <Button>Sign Up</Button>
+                            <Button className="bg-transparent text-neutral-300 font-medium" onClick={() => {}}>Sign Up</Button>
+                        </div>
+                        <div>
+                            <Button className="bg-white px-6 py-2" onClick={() => {}}>Log in</Button>
                         </div>
                     </>
                 </div>
             </div>
+            {children}
         </div>
     )
 };
--- spotify-clone/components/SideBar.tsx
+++ spotify-clone/components/SideBar.tsx
@@ -34,7 +34,7 @@ const SideBar: React.FC<SideBarProps> = ({
 
     return (
         <div className="flex h-full">
-            <div className="hidden md:flex flex-col gap-y-2 bg-black w[300px] p-2">
+            <div className="hidden md:flex flex-col gap-y-2 bg-black w-[300px] p-2">
                 <Box>
                     <div className="flex flex-col gap-y-4 px-5 py-4">
                         {routes.map(item => (
