9220093df5d1744f8fed1a0798c483b313d80b16
serhasHacizade <eren.felek.4@gmail.com>
2023-11-07 15:31:05 +0300

    commit

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
 import './globals.css';
+
 import SideBar from "@/components/SideBar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
 import getSongsByUserId from '@/actions/getSongsByUserId';
+import Player from '@/components/Player';
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -35,6 +37,7 @@ const RootLayout = async({
               <SideBar songs={userSongs}>
                 {children}
               </SideBar>
+              <Player />
             </UserProvider>
           </SupabaseProvider>
       </body>
