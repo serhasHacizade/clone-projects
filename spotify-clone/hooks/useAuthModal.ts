135c35efc3297a804314884a2af7f48a9a5924c6
serhasHacizade <eren.felek.4@gmail.com>
2023-11-28 15:41:03 +0300

    commit

--- spotify-clone/hooks/useAuthModal.ts
+++ spotify-clone/hooks/useAuthModal.ts
@@ -5,7 +5,6 @@ interface AuthModalStore {
     onOpen: () => void;
     onClose: () => void; 
 };
-
 const useAuthModal = create<AuthModalStore> ((set) => ({
     isOpen: false,
     onOpen: () => set({isOpen: true}),
