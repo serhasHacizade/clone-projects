a1461361c72d51cdc9522df86531eb523f2fbb68
serhasHacizade <eren.felek.4@gmail.com>
2023-12-04 11:40:32 +0300

    commit

--- airbnb-clone/src/components/navbar/UserMenu.tsx
+++ airbnb-clone/src/components/navbar/UserMenu.tsx
@@ -23,7 +23,7 @@ const UserMenu = () => {
                 rounded-full hover:bg-neutral-100 transition cursor-pointer">
                     Airbnb your home
                 </div>
-                <div onClick={toggleOpen} className="py-4 md:py-1 md:px-2 border-[1px] border-neutral-200
+                <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200
                 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                     <AiOutlineMenu />
                     <div className="hidden md:block">
@@ -32,7 +32,7 @@ const UserMenu = () => {
                 </div>
             </div>
             {isOpen && (
-                <div className="absolute rounded-xl shadow-md w-[40vw] bg-white overflow-hidden
+                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden
                 right-0 top-12 text-sm">
                     <div className="flex flex-col cursor-pointer">
                         <>
