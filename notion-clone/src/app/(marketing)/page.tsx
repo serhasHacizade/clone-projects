51dca03ddb777b6c658043353ead44d220bf8724
serhasHacizade <eren.felek.4@gmail.com>
2024-02-10 13:58:37 +0300

    commit

--- notion-clone/src/app/(marketing)/page.tsx
+++ notion-clone/src/app/(marketing)/page.tsx
@@ -1,17 +1,18 @@
 import Footer from "./_components/Footer";
 import Heading from "./_components/Heading";
 import Heroes from "./_components/Heroes";
-
+import Navbar from "./_components/Navbar";
 
 const MarketingPage = () => {
     return ( 
         <div className="min-h-full flex flex-col">
             <div className="flex flex-col items-center justify-center md:justify-start
             text-center gap-y-8 flex-1 px-6 pb-10">
+                <Navbar />
                 <Heading />
                 <Heroes />
-                <Footer />
             </div>
+                <Footer />
         </div>
      );
 }
