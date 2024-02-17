e57ada80f8c9bb6b123423c45b9b5a0b7537b716
serhasHacizade <eren.felek.4@gmail.com>
2024-02-17 14:31:35 +0300

    commit

--- notion-clone/src/app/(main)/_components/navigation.tsx
+++ notion-clone/src/app/(main)/_components/navigation.tsx
@@ -3,9 +3,11 @@
 import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState } from "react";
-import {useMediaQuery } from "usehooks-ts"
+import { useMediaQuery } from "usehooks-ts"
 
 import { cn } from "@/lib/utils";
+
+
 
 const Navigation = () => {
     const pathName = usePathname();
@@ -17,12 +19,42 @@ const Navigation = () => {
     const [isResetting, setIsResetting] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+    const handleMouseDown = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        event.preventDefault();
+        event.stopPropagation();
 
+        isResizingRef.current = true;
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+        if (!isResizingRef.current) return;
+        let newWidth = e.clientX;
+
+        if (newWidth < 240) newWidth = 240;
+        if (newWidth > 480) newWidth = 480;
+
+        if (sidebarRef.current && navbarRef.current) {
+            sidebarRef.current.style.width = `${newWidth}px`;
+            navbarRef.current.style.width = `${newWidth}px`;
+            navbarRef.current.style.setProperty("left", `${newWidth}px`);
+            navbarRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
+        };
+    };
+
+    const handleMouseUp = () => {
+        isResizingRef.current = false;
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
+    };
 
     return (
         <>
-            <aside ref={sidebarRef} className={cn("group/sidebar h-full bg-secondary overflow-auto relative flex w-60 flex-col z-[99999]", isResetting && 
-            "transition-all ease-in-out duration-300", isMobile && "w-0")} >
+            <aside ref={sidebarRef} className={cn("group/sidebar h-full bg-gray-200 overflow-y-auto relative flex w-60 flex-col z-[99999]", isResetting &&
+                "transition-all ease-in-out duration-300", isMobile && "w-0")} >
                 <div role="button" className={cn(
                     "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
                     isMobile && "opacity-100")}>
@@ -34,13 +66,13 @@ const Navigation = () => {
                 <div className="mt-4">
                     <p>Documents</p>
                 </div>
-                <div className="opacity-0 group-hover/sidebar:opacity-100 transition
+                <div onMouseDown={handleMouseDown} onClick={() => { }} className="opacity-0 group-hover/sidebar:opacity-100 transition
                 cursor-ew-resize absolute h-full w-1 bg-primary/0 right-0 top-0"/>
             </aside>
             <div ref={navbarRef} className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]", isResetting && "transition-all ease-in-out duration-300",
-            isMobile && "left-0 w-full")}>
+                isMobile && "left-0 w-full")}>
                 <nav className="bg-transparent px-3 py-2 w-full">
-                    {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground"/>}
+                    {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}
                 </nav>
             </div>
         </>
