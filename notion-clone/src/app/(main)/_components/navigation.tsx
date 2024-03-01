ce09fe765f8f1b3e956ef0d7152779fa510b555d
serhasHacizade <eren.felek.4@gmail.com>
2024-03-01 12:11:47 +0300

    commit

--- notion-clone/src/app/(main)/_components/navigation.tsx
+++ notion-clone/src/app/(main)/_components/navigation.tsx
@@ -91,7 +91,7 @@ const Navigation = () => {
 
     return (
         <>
-            <aside ref={sidebarRef} className={cn("group/sidebar h-full bg-gray-50 overflow-y-auto relative flex w-60 flex-col z-[99999]", isResetting &&
+            <aside ref={sidebarRef} className={cn("group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]", isResetting &&
                 "transition-all ease-in-out duration-300", isMobile && "w-0")} >
                 <div onClick={collapse} role="button" className={cn(
                     "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
