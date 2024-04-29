233fdb52319437bf3a378030aaf62d952cde1a0a
serhasHacizade <eren.felek.4@gmail.com>
2024-04-29 11:06:50 +0300

    commit

---
+++ auth-tutorial/components/auth/login-form.tsx
@@ -0,0 +1,13 @@
+import CardWrapper from "./card-wrapper";
+
+
+const LoginForm = () => {
+    return (
+        <CardWrapper headerLabel="Welcome back" backButtonLabel="Don't have an account?"
+        backButtonHref="/auth/register" showSocial>
+            Login Form
+        </CardWrapper>
+    );
+}
+
+export default LoginForm;
