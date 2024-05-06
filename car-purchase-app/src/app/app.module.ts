a6df494dfd912aeb28016416d9938c4cb9ecb076
serhasHacizade <eren.felek.4@gmail.com>
2024-05-06 10:32:25 +0300

    commit

--- car-purchase-app/src/app/app.module.ts
+++ car-purchase-app/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
+import { AvatarModule } from 'primeng/avatar'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    AvatarModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [
     provideClientHydration()
---
+++ car-purchase-app/src/app/components/top-navigation/top-navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-navigation',
+  templateUrl: './top-navigation.component.html',
+  styleUrl: './top-navigation.component.scss'
+})
+export class TopNavigationComponent {
+
+}
