13e74698da44f062cd865a7ba1ea7117ff892f35
serhasHacizade <eren.felek.4@gmail.com>
2024-09-15 12:18:07 +0300

    commit

--- carepulse/app/page.tsx
+++ carepulse/app/page.tsx
@@ -6,17 +6,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image src="/assets/icons/logo-full.svg" height={1000} width={1000} alt="patient" className="mb-12 h-10 w-fit"/>
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePulse</p>
-          </div>
           <Link href="/?admin=true" className="text-green-500">Admin</Link>
+          </div>
         </div>
       </section>
-      <Image src="/assets/images/onboarding-img.png" alt=""/>
+      <Image src="/assets/images/onboarding-img.png" alt="patient" height={1000} width={1000} className="side-img max-w-[50%]"/>
     </div>
   );
 }
