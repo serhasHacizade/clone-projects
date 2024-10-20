483d29ec7d7911d0859e87b01eaa9010bd90a206
serhasHacizade <eren.felek.4@gmail.com>
2024-10-20 09:48:57 +0300

    commit

--- deezer-clone/src/App.vue
+++ deezer-clone/src/App.vue
@@ -1,10 +1,66 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 import Magnify from "vue-material-design-icons/Magnify.vue";
 import Bell from "vue-material-design-icons/Bell.vue";
+
+import SideMenuItem from "./components/SideMenuItem.vue";
+
 </script>
 
 <template>
-  <div id="TopNav" class="fixed right-0 flex items-center justify-between w-[calc(100%-240px)] h-[56px] border-b border-b-[#32323D]"></div>
-  <RouterView />
+  <div
+    id="TopNav"
+    class="fixed right-0 flex items-center justify-between w-[calc(100%-240px)] h-[56px] border-b border-b-[#32323D]"
+  >
+    <div class="flex items-center w-full">
+      <Magnify class="pl-6 mt-1 pr-2" fillColor="#7E7E88" :size="22" />
+      <input
+        type="text"
+        class="pt-1 bg-transparent outline-none font-[300] placeholder-[#BEBEC7] text-[#FFF] w-full max-w-xl"
+        placeholder="Search"
+      />
+    </div>
+    <div class="flex items-center pr-10">
+      <div class="mr-4 p-1 hover:bg-gray-600 rounded-full cursor-pointer">
+        <Bell fillColor="#FFFFFF" :size="20" />
+      </div>
+      <img
+        src="https://yt3.ggpht.com/yti/ANjgQV91DDHMknRuNLgEED2yGho-Qpj8YtY06V-eJGAsWweIVFY=s88-c-k-c0x00ffffff-no-rj"
+        alt=""
+        class="rounded-full w-[33px]"
+      />
+    </div>
+  </div>
+  <div
+    id="SideNav"
+    class="fixed w-[240px] bg-[#191922] h-[100vh] border-r border-r-[#32323D]"
+  >
+    <div class="w-full pl-6 pt-3 cursor-pointer">
+      <RouterLink to="/">
+        <img src="/images/deezer-logo.png" width="130" alt="logo">
+      </RouterLink>
+    </div>
+    <div class="mt-[53px]"></div>
+
+    <SideMenuItem iconString="music" :iconSize="20" pageUrl="/" name="Music"/>
+    <SideMenuItem iconString="podcast" :iconSize="20" pageUrl="/podcasts" name="Podcasts"/>
+    <SideMenuItem iconString="explore" :iconSize="20" pageUrl="/artist" name="Explore"/>
+    <SideMenuItem iconString="favourite" :iconSize="20" pageUrl="/favourite" name="Favourites"/>
+
+    <div class="text-[#A2A2AD] font-light text-[12px] pl-[62px] mt-[25px]">
+      <div class="py-[9px] gover:text-[#EF5465] cursor-pointer">
+        Favourite tracks
+      </div>
+      <div class="py-[9px] gover:text-[#EF5465] cursor-pointer">Playlist</div>
+      <div class="py-[9px] gover:text-[#EF5465] cursor-pointer">Albums</div>
+      <div class="py-[9px] gover:text-[#EF5465] cursor-pointer">Artists</div>
+      <div class="py-[9px] gover:text-[#EF5465] cursor-pointer">Podcasts</div>
+    </div>
+  </div>
+
+  <div
+    class="fixed w-[calc(100%-240px)] h-[calc(100%-56px)] ml-[240px] mt-[56px] overflow-x-auto"
+  >
+    <RouterView />
+  </div>
 </template>
--- deezer-clone/src/views/HomeView.vue
+++ deezer-clone/src/views/HomeView.vue
@@ -1,6 +1,67 @@
 <script setup>
+import MixesInspiredBy from "@/components/MixesInspiredBy.vue";
 </script>
 
 <template>
-  Home
+  <div class="max-w-[1500px] mx-auto">
+    <div class="px-8 mt-8 mi-w-[800px] w-full">
+      <div class="text-white text-xl font-semibold inline-block">
+        Mixes inspired by...
+        <div class="text-sm font-light text-[#A2A2AD]">
+          Discover new tracks similar to ypur favourites
+        </div>
+      </div>
+      <div class="py-3"></div>
+      <div class="flex justify-between w-full gap-8">
+        <div class="xl:w-1/3 w-1/2">
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="VAMPIRE (Oli Sykes of Bring Me The Horizon)"
+            by="Ic3peak"
+            to="/artist"
+            image="https://picsum.photos/id/174/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Deutchland"
+            by="Rammstein"
+            to="/artist"
+            image="https://picsum.photos/id/166/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Fallout"
+            by="Masked Wolf"
+            to="/artist"
+            image="https://picsum.photos/id/127/300/300"
+          />
+        </div>
+      </div>
+      <div class="flex justify-between w-full gap-8">
+        <div class="xl:w-1/3 w-1/2">
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Pray For Pagues"
+            by="Bring Me the Horizon"
+            to="/artist"
+            image="https://picsum.photos/id/201/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="Candy Truck / You Expected"
+            by="Bring Me the Horizon"
+            to="/artist"
+            image="https://picsum.photos/id/202/300/300"
+          />
+          <MixesInspiredBy
+            class="pb-1.5"
+            text="L'efant sauvage"
+            by="Gojira"
+            to="/artist"
+            image="https://picsum.photos/id/212/300/300"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
