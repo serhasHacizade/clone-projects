855f8123e3d93798e098a1b81edd81091fbf1c60
serhasHacizade <eren.felek.4@gmail.com>
2024-10-23 11:45:05 +0300

    commit

--- deezer-clone/src/views/HomeView.vue
+++ deezer-clone/src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CustomCarousel from "@/components/CustomCarousel.vue";
 import MixesInspiredBy from "@/components/MixesInspiredBy.vue";
 import MultiArtistSelect from "@/components/MultiArtistSelect.vue";
 </script>
@@ -150,16 +151,120 @@ import MultiArtistSelect from "@/components/MultiArtistSelect.vue";
       </p>
       <div class="flex justify-center gap-7 w-full">
         <div class="flex items-center justify-center relative">
-          <img width="100" class="absolute -left-36 rounded-full z-0 cursor-pointer" src="https://picsum.photos/id/188/300/300" />
-              <img width="115" class="absolute -left-[80px]  rounded-full z-10 cursor-pointer" src="https://picsum.photos/id/66/300/300" />
-              <img width="160" class="rounded-full z-20 cursor-pointer" src="https://picsum.photos/id/144/300/300" />
-              <img width="115" class="absolute -right-[80px] rounded-full z-10 cursor-pointer" src="https://picsum.photos/id/141/300/300" />
-              <img width="100" class="absolute -right-36 rounded-full z-0 cursor-pointer" src="https://picsum.photos/id/222/300/300" />
+          <img
+            width="100"
+            class="absolute -left-36 rounded-full z-0 cursor-pointer"
+            src="https://picsum.photos/id/188/300/300"
+          />
+          <img
+            width="115"
+            class="absolute -left-[80px] rounded-full z-10 cursor-pointer"
+            src="https://picsum.photos/id/66/300/300"
+          />
+          <img
+            width="160"
+            class="rounded-full z-20 cursor-pointer"
+            src="https://picsum.photos/id/144/300/300"
+          />
+          <img
+            width="115"
+            class="absolute -right-[80px] rounded-full z-10 cursor-pointer"
+            src="https://picsum.photos/id/141/300/300"
+          />
+          <img
+            width="100"
+            class="absolute -right-36 rounded-full z-0 cursor-pointer"
+            src="https://picsum.photos/id/222/300/300"
+          />
         </div>
       </div>
       <div class="w-full flex justify-center p-3">
-        <button type="button" class="text-white text-[15px] font-semibold rounded-full bg-[#EF5465] hover:bg-[#d8384a] px-8 py-2 text-center">ADD ARTISTS</button>
+        <button
+          type="button"
+          class="text-white text-[15px] font-semibold rounded-full bg-[#EF5465] hover:bg-[#d8384a] px-8 py-2 text-center"
+        >
+          ADD ARTISTS
+        </button>
       </div>
+    </div>
+    <div class="mt-8 min-w-[800px]">
+      <CustomCarousel
+        category="New releases for you"
+        :data="[
+          {
+            url: 'https://picsum.photos/id/89/300/300',
+            song: 'Foregone',
+            by: 'In Flames',
+            releasedOn: '10/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/101/300/300',
+            song: 'Dark Waters',
+            by: 'Delain',
+            releasedOn: '09/01/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/121/300/300',
+            song: 'Heritage',
+            by: 'Distant',
+            releasedOn: '10/01/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/134/300/300',
+            song: 'Meanwhile',
+            by: 'Klone',
+            releasedOn: '08/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/144/300/300',
+            song: 'Truth Decay',
+            by: 'You Me At Six',
+            releasedOn: '10/01/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/155/300/300',
+            song: 'This Is Why',
+            by: 'Paramore',
+            releasedOn: '10/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/166/300/300',
+            song: 'The Jaws Of Life',
+            by: 'Pierce The Veil',
+            releasedOn: '09/23/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/198/300/300',
+            song: 'The Future Is Your Past',
+            by: 'In The Brian Jonestown Massacre',
+            releasedOn: '07/25/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/211/300/300',
+            song: 'The Land, The Water, The Sky',
+            by: 'Black Belt Eagle Scout',
+            releasedOn: '09/22/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/223/300/300',
+            song: 'The Day My Father Died',
+            by: 'SYML',
+            releasedOn: '10/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/225/300/300',
+            song: 'Complete Mountain Almanac',
+            by: 'Complete Mountain Almanac',
+            releasedOn: '10/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/239/300/300',
+            song: 'Music From Montana Story',
+            by: 'Kevin Morby',
+            releasedOn: '10/01/2023',
+          },
+        ]"
+      />
     </div>
   </div>
 </template>
