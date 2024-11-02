b39beb8d00016cdd444fa173b42834b7e991c5fe
serhasHacizade <eren.felek.4@gmail.com>
2024-11-02 10:08:50 +0300

    commit

---
+++ deezer-clone/src/views/ArtistView.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import Magnify from 'vue-material-design-icons/Magnify.vue';
+  import Play from 'vue-material-design-icons/Play.vue';
+  import Pause from 'vue-material-design-icons/Pause.vue';
+  import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
+  import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+  import ClockTimeFiveOutline from 'vue-material-design-icons/ClockTimeFiveOutline.vue';
+  import artist from "../artist.json";
+
+  import { useSongStore } from '@/stores/song';
+  import { storeToRefs } from 'pinia';
+
+  const song = useSongStore();
+  const {isPlaying, currentTrack, currentArtist} = storeToRefs(song);
+
+</script>
+
+<template>
+  <div class="max-w-[1500px] mx-auto" id="header-section">
+    
+  </div>
+</template>
--- deezer-clone/src/views/HomeView.vue
+++ deezer-clone/src/views/HomeView.vue
@@ -277,7 +277,90 @@ import Highligths from "@/components/Highlights.vue";
         <div class="rounded-lg w-1/2">
           <Highligths by="Some Artist 1" song="Awesome Song for you" image="https://picsum.photos/id/177/800/300"/>
         </div>
+        <div class="rounded-lg w-1/2">
+          <Highligths by="Some Artist 2" song="Another banger BRUV!" image="https://picsum.photos/id/229/800/300"/>
+        </div>
       </div>
     </div>
+    <div class="mt-8 min-w-[800px]">
+      <CustomCarousel
+        category="New releases for you"
+        :data="[
+          {
+            url: 'https://picsum.photos/id/89/300/300',
+            song: 'Foregone',
+            by: 'In Flames',
+            releasedOn: '10/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/101/300/300',
+            song: 'Dark Waters',
+            by: 'Delain',
+            releasedOn: '09/01/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/121/300/300',
+            song: 'Heritage',
+            by: 'Distant',
+            releasedOn: '10/01/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/134/300/300',
+            song: 'Meanwhile',
+            by: 'Klone',
+            releasedOn: '08/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/144/300/300',
+            song: 'Truth Decay',
+            by: 'You Me At Six',
+            releasedOn: '10/01/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/155/300/300',
+            song: 'This Is Why',
+            by: 'Paramore',
+            releasedOn: '10/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/166/300/300',
+            song: 'The Jaws Of Life',
+            by: 'Pierce The Veil',
+            releasedOn: '09/23/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/198/300/300',
+            song: 'The Future Is Your Past',
+            by: 'In The Brian Jonestown Massacre',
+            releasedOn: '07/25/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/211/300/300',
+            song: 'The Land, The Water, The Sky',
+            by: 'Black Belt Eagle Scout',
+            releasedOn: '09/22/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/223/300/300',
+            song: 'The Day My Father Died',
+            by: 'SYML',
+            releasedOn: '10/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/225/300/300',
+            song: 'Complete Mountain Almanac',
+            by: 'Complete Mountain Almanac',
+            releasedOn: '10/02/2023',
+          },
+          {
+            url: 'https://picsum.photos/id/239/300/300',
+            song: 'Music From Montana Story',
+            by: 'Kevin Morby',
+            releasedOn: '10/01/2023',
+          },
+        ]"
+      />
+    </div>
+    <div class="pb-40"></div>
   </div>
 </template>
